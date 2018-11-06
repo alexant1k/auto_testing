bcc34bd0ddf0d3d174eac378220b0c8d64d245de
alexant1k <34940330+alexant1k@users.noreply.github.com>
2018-11-06 14:46:59 +0300

    Добавление методов в классы

--- sandbox/src/main/java/ru/autotest/sandbox/Rectangle.java
+++ sandbox/src/main/java/ru/autotest/sandbox/Rectangle.java
@@ -1,12 +1,17 @@
 package ru.autotest.sandbox;
 
 public class Rectangle {
-    public  double a;
-    public  double b;
-    public  Rectangle(double a, double b){
+    public double a;
+    public double b;
+
+    public Rectangle(double a, double b) {
         this.a = a;
         this.b = b;
 
 
+    }
+
+    public double area() {
+        return this.a * this.b;
     }
 }
